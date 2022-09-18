a432d25ae77173a1d0e236492b6a1ee9c116ec4a
Mariyamahnoor95 <mariyammahnoor@gmail.com>
2022-09-18 23:36:49 +0500

    container complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,8 +16,32 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Wellcome to My App "),
       ),
-      body: Container(
-        child: Center(child: Text("hi mahnoor")),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(10),
+          alignment: Alignment.center,
+          width: 100,
+          height: 100,
+          decoration: BoxDecoration(
+            color: Colors.blueAccent,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [ BoxShadow(
+                  color: Colors.grey[400],
+                  blurRadius: 5,
+                  offset: const Offset(2.0, 10.0))
+            ],
+           
+            gradient:
+                LinearGradient(colors: [Colors.black, Colors.yellow.shade400]),
+          ),
+          child: const Text("hey there",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 20,
+          ),
+        ),
       ),
     );
   }
