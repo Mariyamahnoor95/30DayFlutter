2f5dbe488238b33b5cd3b7d4e9d9156fb929ba03
Mariyamahnoor95 <mariyammahnoor@gmail.com>
2022-10-17 19:36:20 +0500

    json decoding means string to map

--- lib/pages/catalog_page.dart
+++ lib/pages/catalog_page.dart
@@ -2,6 +2,7 @@ import 'package:awesome_app/models/catalog.dart';
 import 'package:awesome_app/widgets/drawer.dart';
 import 'package:awesome_app/widgets/item_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -13,17 +14,31 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    var decodeData = jsonDecode(catalogJson); //string to map
+    final productData = decodeData["products"];
+    print(productData);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final dummylist = List.generate(20, ((index) => CatalogModel.items[0]));
+    // final dummylist = List.generate(20, ((index) => CatalogModel.items[0]));
+
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
         title: const Text("catalog page "),
       ),
       body: ListView.builder(
-        itemCount: dummylist.length,
+        itemCount: CatalogModel.items.length,
         itemBuilder: (context, index) {
-          return ItemWidget(item: dummylist[index]);
+          return ItemWidget(item: CatalogModel.items[index]);
         },
       ),
       drawer: DrawerSide(),
